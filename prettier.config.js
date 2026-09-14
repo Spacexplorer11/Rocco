01289ca2687fc4815b827ca0cc53c5c9b2881ea3
Akaalroop Singh <akaal@akaalroop.com>
2026-09-14 22:34:51 +0100

    Ran wrangler types again and excluded from prettier

--- prettier.config.js
+++ prettier.config.js
@@ -5,7 +5,10 @@ const config = {
 	trailingComma: "none",
 	printWidth: 100,
 	plugins: ["prettier-plugin-svelte", "prettier-plugin-tailwindcss"],
-	overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
+	overrides: [
+		{ files: "*.svelte", options: { parser: "svelte" } },
+		{ excludeFiles: "worker-configuration.d.ts" }
+	],
 	tailwindStylesheet: "./src/routes/layout.css"
 };
 
