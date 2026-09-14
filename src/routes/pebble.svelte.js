86c90f9d20343e2cb6b74d8b9efdb3f4d3f6a5a2
Akaalroop Singh <akaal@akaalroop.com>
2026-09-14 22:01:34 +0100

    Added defence against if localStorage fails

--- src/routes/pebble.svelte.js
+++ src/routes/pebble.svelte.js
@@ -1,9 +1,16 @@
 export const pebbles = $state({ value: 0 });
 
+/** This only runs during load */
 export function load_pebbles() {
 	if (typeof window === "undefined") return;
 	if (typeof localStorage === "undefined") return;
-	const stored = Number(localStorage.getItem("pebbles"));
+	let stored = 0;
+	try {
+		stored = Number(localStorage.getItem("pebbles"));
+	} catch (error) {
+		console.error("An error occurred fetching the pebbles value from localStorage");
+		return;
+	}
 	if (Number.isNaN(stored) || !Number.isFinite(stored) || stored < 0) {
 		pebbles.value = 0;
 		console.log("An error occurred with the pebbles value, it was invalid. Resetting to 0.");
