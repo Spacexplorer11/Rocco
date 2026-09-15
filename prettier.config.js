f4354b44ebe6268119ea46164b99ebf62cda5b38
Akaalroop Singh <akaal@akaalroop.com>
2026-09-15 18:38:36 +0100

    Maybe now coderabbit won't have any issues???

--- prettier.config.js
+++ prettier.config.js
@@ -5,10 +5,7 @@ const config = {
 	trailingComma: "none",
 	printWidth: 100,
 	plugins: ["prettier-plugin-svelte", "prettier-plugin-tailwindcss"],
-	overrides: [
-		{ files: "*.svelte", options: { parser: "svelte" } },
-		{ excludeFiles: "worker-configuration.d.ts" }
-	],
+	overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
 	tailwindStylesheet: "./src/routes/layout.css"
 };
 
