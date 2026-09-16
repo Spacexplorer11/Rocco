e0fd438869036f4e922da7ecb07823434611d855
Akaalroop Singh <akaal@akaalroop.com>
2026-09-16 20:28:10 +0100

    Changed the rock_name variable to a rock object with a property of name

--- src/routes/rock.svelte.ts
+++ src/routes/rock.svelte.ts
@@ -1,11 +1,11 @@
-export let rock_name = $state("");
+export let rock = $state({ name: "" });
 
 export function save_rock_name() {
 	if (typeof window === "undefined") return;
 	if (typeof localStorage === "undefined") return;
 	try {
-		localStorage.setItem("rock_name", rock_name);
-		console.log("Successfully saved rock name as ", rock_name);
+		localStorage.setItem("rock_name", rock.name);
+		console.log("Successfully saved rock name as ", rock.name);
 	} catch (error) {
 		console.error("An error occurred when saving the rock name");
 	}
@@ -15,8 +15,8 @@ export function load_rock_name() {
 	if (typeof window === "undefined") return;
 	if (typeof localStorage === "undefined") return;
 	try {
-		rock_name = localStorage.getItem("rock_name") ?? "";
-		console.log("Successfully loaded rock name as ", rock_name);
+		rock.name = localStorage.getItem("rock_name") ?? "";
+		console.log("Successfully loaded rock name as ", rock.name);
 	} catch (error) {
 		console.error("An error occurred when saving the rock name");
 	}
