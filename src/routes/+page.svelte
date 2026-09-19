2a778422aa37d042bd01c5d5cacdfd9ebab184bd
ferboinkles <ferboinkles@gmail.com>
2026-09-19 19:05:45 +0100

    Fixed some padding bugs, added the picture and cropped it and figured out how to fix it all

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { rock } from "./rock.svelte.ts";
+	import { goto } from '$app/navigation';
 </script>
 
 <h1 class="text-center text-5xl text-gray-700">Rocco</h1>
@@ -11,7 +12,7 @@
 
 <div class=" mx-auto flex flex-col items-center justify-center text-center">
 	<input maxlength="30"
-		class=" mx-auto rounded-4xl fill-black text-gray-600 disabled:cursor-not-allowed"
+		class=" mx-auto rounded-4xl fill-black text-white disabled:cursor-not-allowed"
 		bind:value={rock.name}
 	/>
 
@@ -36,12 +37,12 @@
 			border: 2px solid #2d848a;
 			border-radius: 99999px;
 			outline: #267071 solid 2px;
-			background: #27812a;
-		}
-	</style>
-	<h2 class="text-center text-2xl text-amber-900">Note - rock name cannot be longer than 30 characters and cannot be blank</h2>
+			background: #27812a;}
 
-	{#if rock.name != null}
-		<button class="m-20 text-2xl text-green-500 text-center">Confirm?</button>
+	</style>
+	<h2 class="text-center text-2xl text-amber-600">Note - rock name cannot be longer than 30 characters and cannot be blank</h2>
+
+	{#if rock.name}
+		<button class="m-20 text-2xl text-green-500 text-center" onclick={() => goto("/home")}>Confirm?</button>
 	{/if}
 </div>
