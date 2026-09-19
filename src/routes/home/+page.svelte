4dd285d27f5120e8a61ddb356d48d8764395b021
ferboinkles <ferboinkles@gmail.com>
2026-09-19 19:05:45 +0100

    Fixed some padding bugs, added the picture and cropped it and figured out how to fix it all

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+<button class="flex-row flex whitespace-normal mt-60 mx-auto m-10 justify-center">
+        <img alt="Rocco!" src="images/rocco.png" />
+</button>
+<div class="flex-row flex">
+<button class="rounded-4xl p-5 mr-[84vw] mt-52 ml-[1vw] text-left bg-linear-to-r from-[#63A46C] to-[#16DB93]">Go to shop</button>
+<button class="rounded-4xl p-5 ml-[1vw]  mt-52 mb-auto text-right bg-linear-to-l from-[#63A46C] to-[#16DB93]">Settings</button>
+</div>
