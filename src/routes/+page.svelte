9065ba2daebf43f6e95e5446b6debd83d85f988a
Akaalroop Singh <akaal@akaalroop.com>
2026-09-22 22:10:44 +0100

    Change shop UI to be nice and grid for future and add semantic <header> element

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,36 +11,40 @@
 	});
 </script>
 
-<h1 class="text-center text-5xl text-gray-700">Rocco</h1>
-<h2 class="text-center text-3xl text-amber-900">Your little friend that rocks!</h2>
+<header>
+	<h1 class="text-center text-5xl text-gray-700">Rocco</h1>
+	<h2 class="text-center text-3xl text-amber-900">Your little friend that rocks!</h2>
+</header>
 
-<h3 class="p-40 text-center text-4xl text-white hover:animate-pulse">
-	Welcome to Rocco! What would you like to name your pet rock?
-</h3>
+<main>
+	<h3 class="p-40 text-center text-4xl text-white hover:animate-pulse">
+		Welcome to Rocco! What would you like to name your pet rock?
+	</h3>
 
-<div class=" mx-auto flex flex-col items-center justify-center text-center">
-	<input
-		maxlength="30"
-		class=" mx-auto rounded-4xl fill-green-500 text-white disabled:cursor-not-allowed"
-		id="rock-name-input"
-		bind:value={rock.name}
-	/>
-	<label for="rock-name-input" class="text-center text-2xl text-amber-600"
-		>Note - rock name cannot be longer than 30 characters and cannot be blank</label
-	>
-
-	{#if rock.name.trim().length > 0}
-		<button
-			class="m-20 rounded-4xl bg-linear-to-r from-[#63A46C] to-[#16DB93] p-5 text-center text-2xl text-blue-800"
-			onclick={() => {
-				rock.happiness.total = 100;
-				rock.happiness.affection = 100;
-				rock.happiness.nutrition = 100;
-				goto("/home");
-			}}>Confirm?</button
+	<div class=" mx-auto flex flex-col items-center justify-center text-center">
+		<input
+			maxlength="30"
+			class=" mx-auto rounded-4xl fill-green-500 text-white disabled:cursor-not-allowed"
+			id="rock-name-input"
+			bind:value={rock.name}
+		/>
+		<label for="rock-name-input" class="text-center text-2xl text-amber-600"
+			>Note - rock name cannot be longer than 30 characters and cannot be blank</label
 		>
-	{/if}
-</div>
+
+		{#if rock.name.trim().length > 0}
+			<button
+				class="m-20 rounded-4xl bg-linear-to-r from-[#63A46C] to-[#16DB93] p-5 text-center text-2xl text-blue-800"
+				onclick={() => {
+					rock.happiness.total = 100;
+					rock.happiness.affection = 100;
+					rock.happiness.nutrition = 100;
+					goto("/home");
+				}}>Confirm?</button
+			>
+		{/if}
+	</div>
+</main>
 
 <style>
 	input {
--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -5,20 +5,6 @@
 	import { goto } from "$app/navigation";
 	import { onMount } from "svelte";
 
-	const itemImages = import.meta.glob("/src/lib/images/items/*.png", { eager: true, import: "default" }) as Record<
-		string,
-		string
-	>;
-
-	export function getItemImage(assetPath: string): string {
-		const url = itemImages[assetPath];
-		if (!url) {
-			console.error(`No image found for asset_path: ${assetPath}`);
-			return ""; // or a fallback placeholder image
-		}
-		return url;
-	}
-
 	onMount(() => {
 		load_rock();
 		if (rock.name.trim().length === 0) {
@@ -37,32 +23,43 @@
 	});
 </script>
 
-<h1 class="text-center text-5xl text-black">Shop: You have {pebbles.value} pebbles</h1>
-
-<button
-	class="mt-25 mr-[83vw] ml-[1vw] rounded-4xl bg-linear-to-r from-[#63A46C] to-[#16DB93] p-5 text-left"
-	onclick={() => goto("/home")}>Back</button
->
-
-{#each items.possible_items as item (item.id)}
-	<h2 class="text-center text-3xl text-black">{item.name} - {item.price} pebbles</h2>
-	<img
-		class="m-10 mx-auto mt-40 flex flex-row justify-center whitespace-normal"
-		alt={item.name}
-		title={item.name}
-		src={item.asset_path}
-	/>
+<header class="mb-10 flex flex-row">
 	<button
-		class="mx-auto bg-linear-to-r from-[#63A46C] to-[#16DB93]"
-		id={item.id}
-		title="Buy {item.name} for {item.price} pebbles"
-		onclick={() => {
-			if (pebbles.value >= item.price) {
-				pebbles.value -= item.price;
-				items.bought_items.push(item);
-			}
-		}}
+		class="left-3 rounded-4xl bg-linear-to-r from-[#63A46C] to-[#16DB93] p-5 text-left"
+		onclick={() => goto("/home")}>Back</button
 	>
-		{pebbles.value >= item.price ? "Buy Now!" : "Can't afford it!"}
-	</button>
-{/each}
+	<h1 class="mx-auto text-center text-5xl text-black">Shop: You have {pebbles.value} pebbles</h1>
+</header>
+
+<main class="m-2 grid grid-cols-3 gap-3">
+	{#each items.possible_items as item (item.id)}
+		<div class="@container flex flex-col">
+			<h2 class="text-center text-3xl text-black">{item.name} - {item.price} pebbles</h2>
+			<img
+				class="m-10 mx-auto mt-40 justify-center whitespace-normal"
+				alt={item.name}
+				title={item.name}
+				src={item.asset_path}
+			/>
+			<button
+				class="mx-auto justify-center rounded-4xl p-4 text-center active:bg-linear-to-r active:from-[#63A46C] active:to-[#16DB93] disabled:cursor-not-allowed disabled:bg-gray-400"
+				id={item.id}
+				title="Buy {item.name} for {item.price} pebbles"
+				disabled={items.bought_items.includes(item) || pebbles.value < item.price}
+				onclick={() => {
+					if (pebbles.value >= item.price) {
+						pebbles.value -= item.price;
+						items.bought_items.push(item);
+						console.log(`Bought ${item.name}`);
+					}
+				}}
+			>
+				{items.bought_items.includes(item)
+					? "You already have this!"
+					: pebbles.value >= item.price
+						? "Buy Now!"
+						: "Can't afford this!"}
+			</button>
+		</div>
+	{/each}
+</main>
