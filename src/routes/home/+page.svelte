17a9b6b1d1312dcd2d8c81e0bc56a330997ab841
Akaalroop <akaal@akaalroop.com>
2026-09-23 13:39:42 +0000

    The food button and click on the rock to pet it text is now part of the header on the home page and adapts accordingly

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -12,39 +12,38 @@
 	});
 </script>
 
-<h1 class=" flex flex-row justify-center text-center text-4xl text-black">{rock.name}</h1>
-<div class="flex flex-row justify-between">
-	<div class="flex w-full flex-col items-center justify-center">
-		<span class="mb-2 text-3xl text-yellow-400">Happiness = {rock.happiness.total}</span>
-		<div class="h-4 w-full max-w-md overflow-hidden rounded-full border-2">
-			<div class="h-full bg-yellow-400" style="width: {rock.happiness.total}%;"></div>
+<header>
+	<h1 class=" flex flex-row justify-center text-center text-4xl text-black">{rock.name}</h1>
+	<div class="flex flex-row justify-between">
+		<div class="flex w-full flex-col items-center">
+			<span class="mb-2 text-center text-3xl text-yellow-400">Happiness = {rock.happiness.total}</span>
+			<div class="h-4 w-full max-w-md overflow-hidden rounded-full border-2">
+				<div class="h-full bg-yellow-400" style="width: {rock.happiness.total}%;"></div>
+			</div>
+		</div>
+		<div class="flex w-full flex-col items-center">
+			<span class="mb-2 text-center text-3xl text-pink-600">Affection = {rock.happiness.affection}</span>
+			<div class="h-4 w-full max-w-md overflow-hidden rounded-full border-2">
+				<div class="h-full bg-pink-600" style="width: {rock.happiness.affection}%;"></div>
+			</div>
+			<p class="text-center text-3xl whitespace-normal">Click on the rock to pet it</p>
+		</div>
+		<div class="flex w-full flex-col items-center">
+			<span class="mb-2 text-center text-3xl text-blue-500">Nutrition = {rock.happiness.nutrition}</span>
+			<div class="h-4 w-full max-w-md overflow-hidden rounded-full border-2">
+				<div class="h-full bg-blue-500" style="width: {rock.happiness.nutrition}%;"></div>
+			</div>
+			<button
+				class="max-w-fit rounded-4xl bg-linear-to-r from-[#63A46C] to-[#16DB93] p-5 text-3xl"
+				onclick={() => {
+					if (rock.happiness.nutrition < 100) {
+						rock.happiness.nutrition += 1;
+					}
+				}}>Food</button
+			>
 		</div>
 	</div>
-	<div class="flex w-full flex-col items-center justify-center">
-		<span class="mb-2 text-3xl text-pink-600">Affection = {rock.happiness.affection}</span>
-		<div class="h-4 w-full max-w-md overflow-hidden rounded-full border-2">
-			<div class="h-full bg-pink-600" style="width: {rock.happiness.affection}%;"></div>
-		</div>
-	</div>
-	<div class="flex w-full flex-col items-center justify-center">
-		<span class="mb-2 text-3xl text-blue-500">Nutrition = {rock.happiness.nutrition}</span>
-		<div class="h-4 w-full max-w-md overflow-hidden rounded-full border-2">
-			<div class="h-full bg-blue-500" style="width: {rock.happiness.nutrition}%;"></div>
-		</div>
-	</div>
-</div>
-<div class="flex flex-row">
-	<p class=" mr-[15vw] ml-[36vw] text-center text-3xl whitespace-normal">Click on the rock to pet it</p>
-
-	<button
-		class="justify-right rounded-4xl bg-linear-to-r from-[#63A46C] to-[#16DB93] p-5 text-right text-3xl"
-		onclick={() => {
-			if (rock.happiness.nutrition < 100) {
-				rock.happiness.nutrition += 1;
-			}
-		}}>Food</button
-	>
-</div>
+</header>
 
 <button
 	class="m-10 mx-auto mt-40 flex flex-row justify-center whitespace-normal"
